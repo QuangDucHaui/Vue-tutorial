6ab43d308531d427a922fb8921ede15b8602f3aa
QuangDucHaui <dangquangduc210421dte@gmail.com>
2024-09-11 18:33:30 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<about_page></about_page>
+<Homeview></Homeview>
+</div>
+</template>
+
+<script setup>
+import about_page from './components/About.vue';
+import Homeview from './components/Homeview.vue';
+import { ref } from 'vue';
+const checked = ref(true)
+
+const check = ref(false)
+</script>
+<style scoped >
+
+</style>
+
---
+++ src/components/Homeview.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="about">
+    <h1>this is about page</h1>
+  </div>
+</template>
+<script setup> 
+
+</script>
