bea7d3af405bd591654a5602b721371fabed7e05
Quang Duc <99651708+QuangDucHaui@users.noreply.github.com>
2024-09-12 07:44:31 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 <div>
 <about_page></about_page>
 <Homeview></Homeview>
+<h1>{{ message }}</h1>
 </div>
 </template>
 
@@ -10,7 +11,7 @@ import about_page from './components/About.vue';
 import Homeview from './components/Homeview.vue';
 import { ref } from 'vue';
 const checked = ref(true)
-
+const message = ref('hello')
 const check = ref(false)
 </script>
 <style scoped >
